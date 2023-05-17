c0bb2062722bac7cd436608575d619476958e5df
Jenocke <jenocke@gmail.com>
2023-05-17 21:40:18 +0200

    first step of file processing done. The file is changed into a string array. Each string is then separated into segments. Process know when it's a watch or not

--- src/utils/controllers.js
+++ src/utils/controllers.js
@@ -1,13 +1,47 @@
 const processTextFile = (fileContent) => {
+  const staffWatchIds = ["001", "002", "003"];
+  const leg1Day = "11";
+  const leg2Day = "12";
   //TODO
   /* slice the text by lines
       process each line
           slice the line in segments
-          process each segment
-          return an object with the segments data
+          check the first segment's data
+            if it's starting by 0056 => new musher
+              start new object
+              process next lines until another 056 => close musher
+          return the object
     return an array of objects
    */
   const linesArray = fileContent.split("\r\n");
+
+  //main loop. Loop through the whole file. We exit when we reach the "End" part or when we reach the end of the file
+  let i = 0;
+  while (i < linesArray.length && !linesArray[i].startsWith("End")) {
+    //can only be a new musher
+    const lineSegments = linesArray[i].split(" ");
+    //checking that it's a watch, just to be sure
+    const isAWatch = lineSegments[0].startsWith("056");
+    if (isAWatch) {
+      const watchId = lineSegments[0].slice(lineSegments[0].length - 3);
+      //checking if it's a staff's watch, to be sure
+      const isStaffWatch = staffWatchIds.includes(watchId);
+      if (!isStaffWatch) {
+        //this is a watch ID, and it's a musher's watch => we can start creating the object
+        const musher = { musherNumber: watchId };
+        if (lineSegments[1].startsWith(leg1Day)) {
+          musher.arrivalDay = 1;
+        } else if (lineSegments[1].startsWith(leg2Day)) {
+          musher.arrivalDay = 2;
+        }
+        musher.scanningTime =
+          lineSegments[2].slice(0, 2) + ":" + lineSegments[2].slice(2, 4);
+        //subloop. that loop goes through an arrival until the staff's watch is scanned
+        console.log(musher);
+      }
+    }
+    i++;
+  }
 };
 
 const handleImport = () => {
